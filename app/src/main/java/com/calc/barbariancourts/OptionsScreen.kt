61f3accfcefe1bf209ccff67993091659d2d4a08
Daniel Tibães Nogueira <daniel.tibaes@yahoo.com>
2023-02-28 21:53:41 -0300

    Under-Construction

---
+++ app/src/main/java/com/calc/barbariancourts/OptionsScreen.kt
@@ -0,0 +1,11 @@
+package com.calc.barbariancourts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OptionsScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_options_screen)
+    }
+}
